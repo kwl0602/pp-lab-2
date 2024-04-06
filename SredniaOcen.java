7433ee42632ed81581a4ec65fbbff22728d0f358
unknown <daavwid2001@gmail.com>
2024-04-06 12:10:05 +0200

    Initial commit of all files to the repository

---
+++ SredniaOcen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SredniaOcen {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Program do obliczania średniej arytmetycznej z ocen.");
+
+            // Pobieranie ocen od użytkownika
+            System.out.print("Wprowadź ocenę z matematyki: ");
+            double matematyka = scanner.nextDouble();
+
+            System.out.print("Wprowadź ocenę z fizyki: ");
+            double fizyka = scanner.nextDouble();
+
+            System.out.print("Wprowadź ocenę z chemii: ");
+            double chemia = scanner.nextDouble();
+
+            // Obliczanie średniej
+            double srednia = (matematyka + fizyka + chemia) / 3;
+
+            // Wyświetlanie wyniku
+            System.out.printf("Średnia arytmetyczna z ocen: %.2f", srednia);
+        }
+    }
+}
